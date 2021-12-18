29dae73a5fb143dc941d535b12ea0fc925edc7b5
Krista <k.marie.koch@gmail.com>
2021-12-18 11:34:17 -0600

    Add text mode

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ import (
 
 const port = 1313
 
-const limit = 100
+const pageSize = 100
 
 const (
 	errServerError = "server error"
@@ -34,25 +34,38 @@ const (
 	spotifyAPIBase     = "https://api.spotify.com/v1"
 	spotifyAuthAPIBase = "https://accounts.spotify.com/api"
 
-	modeMusicVideo = "music video"
-	modeLive       = "live"
-	modeCover      = "cover"
-	modeShit       = "shit"
+	searchModeMusicVideo = "music video"
+	searchModeLive       = "live"
+	searchModeCover      = "cover"
+	searchModeShit       = "shit"
+
+	modeWeb  = "web"
+	modeText = "text"
 )
 
 type config struct {
 	spotifyClientID     string
 	spotifyClientSecret string
 	mode                string
+	searchMode          string
 }
 
-type handler struct {
-	spotifyClientID     string
-	spotifyClientSecret string
-	token               string
-	mode                string
-	tokenSetAt          *time.Time
-	che                 map[string]videoPage
+func (cfg *config) validate() error {
+	if cfg.spotifyClientID == "" {
+		return errors.New("spotify client id is empty")
+	}
+	if cfg.spotifyClientSecret == "" {
+		return errors.New("spotify client secret empty")
+	}
+
+	return nil
+}
+
+type videoPlaylist struct {
+	cfg        config
+	token      string
+	tokenSetAt *time.Time
+	che        map[string]videoPage
 }
 
 type tmpldata struct {
@@ -64,8 +77,8 @@ type tmpldata struct {
 }
 
 type videoPage struct {
-	videos     []video
-	isNextPage bool
+	videos  []video
+	hasNext bool
 }
 
 type video struct {
@@ -93,19 +106,15 @@ type playlistRes struct {
 func main() {
 	cfg := config{}
 
-	flag.StringVar(&cfg.spotifyClientID, "spotifyClientID", "", "spotify client id")
-	flag.StringVar(&cfg.spotifyClientSecret, "spotifyClientSecret", "", "spotify client secret")
-	flag.StringVar(&cfg.mode, "mode", modeMusicVideo, "search mode (default is music video)")
+	flag.StringVar(&cfg.mode, "mode", modeWeb, "app mode (web or text, default is web)")
+	flag.StringVar(&cfg.searchMode, "searchMode", searchModeMusicVideo, "search mode (music video, live, cover, or shit, default is music video)")
+	flag.StringVar(&cfg.spotifyClientID, "spotifyClientID", os.Getenv("SPOTIFY_CLIENT_ID"), "spotify client id")
+	flag.StringVar(&cfg.spotifyClientSecret, "spotifyClientSecret", os.Getenv("SPOTIFY_CLIENT_SECRET"), "spotify client secret")
 	flag.Parse()
 
-	cfg.spotifyClientID = firstString(os.Getenv("SPOTIFY_CLIENT_ID"), cfg.spotifyClientID)
-	cfg.spotifyClientSecret = firstString(os.Getenv("SPOTIFY_CLIENT_SECRET"), cfg.spotifyClientSecret)
-
-	if cfg.spotifyClientID == "" {
-		log.Fatal("spotify client id is empty")
-	}
-	if cfg.spotifyClientSecret == "" {
-		log.Fatal("spotify client secret empty")
+	var err error
+	if err = cfg.validate(); nil != err {
+		log.Fatal(err)
 	}
 
 	log.Printf(`
@@ -113,29 +122,112 @@ func main() {
 		client id:     %s
 		client secret: %s
 		mode:          %s 
+		search mode:   %s
 	`,
 		cfg.spotifyClientID,
 		cfg.spotifyClientSecret,
 		cfg.mode,
+		cfg.searchMode,
 	)
 
-	cache := make(map[string]videoPage)
+	var vp videoPlaylist
+	vp.cfg = cfg
+	vp.che = make(map[string]videoPage)
 
-	h := handler{
-		spotifyClientID:     cfg.spotifyClientID,
-		spotifyClientSecret: cfg.spotifyClientSecret,
-		che:                 cache,
-		mode:                cfg.mode,
+	switch cfg.mode {
+	case modeWeb:
+		vp.handleWeb()
+	case modeText:
+		vp.handleText()
+	default:
+		log.Fatalf("unknown mode '%s'", cfg.mode)
 	}
+}
 
+func (vp *videoPlaylist) handleWeb() {
 	r := mux.NewRouter()
-	r.Handle("/", &h).Methods("GET", "POST")
+	r.Handle("/", vp).Methods("GET", "POST")
 
 	log.Printf("🧜‍♂️  server is running at http://localhost:%d", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
 }
 
-func (c *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (vp *videoPlaylist) handleText() {
+	var err error
+
+	for {
+		var plURI string
+		for {
+			time.Sleep(500 * time.Millisecond)
+			fmt.Printf("Enter a spotify playlist uri: ")
+
+			if _, err = fmt.Scanln(&plURI); nil != err {
+				log.Fatal(err)
+			}
+
+			if strings.TrimSpace(plURI) == "" {
+				fmt.Println("Please enter a valid uri")
+				continue
+			}
+
+			break
+		}
+
+		pageNum := 1
+		for {
+			offset := (pageNum - 1) * pageSize
+
+			var songs []string
+			var hasNextPage bool
+			if songs, hasNextPage, err = vp.getPlaylistSongs(plURI, offset); nil != err {
+				log.Fatal(err)
+			}
+
+			videos := vp.buildSearchURLs(songs)
+
+			for i, v := range videos {
+				// 94.  Wise Up by Aimee Mann → https://www.youtube.com/results?search_query=Wise+Up+by+Aimee+Mann+music+video
+				fmt.Printf(
+					"%d. %s %s %s → %s \n",
+					offset+i+1,
+					"\033[31m", // color red
+					v.Name,
+					"\033[0m", // color reset
+					v.SearchURL,
+				)
+			}
+
+			if !hasNextPage {
+				break
+			}
+
+			var choice string
+			for {
+				fmt.Printf("Hit enter for the next page, or type n to choose a new playlist: ")
+
+				// Debug: rogers playlist uri -> 16FifVITGI0ud84IuTgj03
+				if _, err = fmt.Scanln(&choice); nil != err && err.Error() != "unexpected newline" {
+					log.Fatal(err)
+				}
+
+				if strings.TrimSpace(choice) == "" {
+					pageNum++
+					break
+				} else if strings.TrimSpace(choice) == "n" {
+					break
+				} else {
+					fmt.Printf("unknown choice '%s'\n", choice)
+				}
+			}
+
+			if choice == "n" {
+				break
+			}
+		}
+	}
+}
+
+func (vp *videoPlaylist) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var err error
 
 	if err = r.ParseForm(); nil != err {
@@ -150,7 +242,7 @@ func (c *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if strings.TrimSpace(plURI) != "" {
 		pageNumStr := r.Form.Get("page")
 
-		if d, err = c.buildTemplateData(plURI, pageNumStr); nil != err {
+		if d, err = vp.buildTemplateData(plURI, pageNumStr); nil != err {
 			log.Printf("error building template data, err %s", err)
 
 			if err.Error() == errNotFound {
@@ -173,7 +265,7 @@ func (c *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index", d)
 }
 
-func (c *handler) buildTemplateData(
+func (vp *videoPlaylist) buildTemplateData(
 	plURI string,
 	pageNumStr string,
 ) (tmpldata, error) {
@@ -189,54 +281,40 @@ func (c *handler) buildTemplateData(
 		pageNum = 1
 	}
 
-	offset := (pageNum - 1) * limit
+	offset := (pageNum - 1) * pageSize
 
-	// Request song data, creating a group of song+artist strings.
-	// Before requestins, check to see if we have this uri:page cached.
 	key := fmt.Sprintf("%s:%d", plURI, pageNum)
-	results, ok := c.che[key]
+	results, ok := vp.che[key]
 	if ok {
 		log.Printf("cache hit with key %s", key)
 	}
 	if !ok {
 		log.Printf("cache miss with key %s, will fetch video data", key)
 
-		// Request an oauth token.
-		// Before requesting, check to see we already have an unexpired token.
-		now := time.Now()
-		if c.tokenSetAt == nil || now.Sub(*c.tokenSetAt) > 60*time.Minute {
-			if c.token, err = c.getSpotifyToken(); nil != err {
-				return d, err
-			}
-		} else {
-			tokenAge := now.Sub(*c.tokenSetAt).Minutes()
-			log.Printf("reusing %.0fmin old token, will expire in %.0fmin", tokenAge, 60-tokenAge)
-		}
-
 		var songs []string
-		var isNextPage bool
-		if songs, isNextPage, err = c.getPlaylistSongs(plURI, offset); nil != err {
+		var hasNextPage bool
+		if songs, hasNextPage, err = vp.getPlaylistSongs(plURI, offset); nil != err {
 			return d, err
 		}
 
-		// Build the youtube search urls.
-		videos := c.buildVideos(songs)
+		videos := vp.buildSearchURLs(songs)
+
 		results = videoPage{
-			videos:     videos,
-			isNextPage: isNextPage,
+			videos:  videos,
+			hasNext: hasNextPage,
 		}
 
-		c.che[key] = results
+		vp.che[key] = results
 	}
 
-	// We know there's a previous page of songs if sutracting
+	// We know there's a previous page of songs if subtracting
 	// the limit is above 0. We know there's a next if the API
 	// returned a Next value in the playlist API response.
 	var prevURL, nextURL string
-	if offset-limit > -1 {
+	if offset-pageSize > -1 {
 		prevURL = fmt.Sprintf("/?uri=%s&page=%d", plURI, pageNum-1)
 	}
-	if results.isNextPage {
+	if results.hasNext {
 		nextURL = fmt.Sprintf("/?uri=%s&page=%d", plURI, pageNum+1)
 	}
 
@@ -250,7 +328,7 @@ func (c *handler) buildTemplateData(
 	return d, nil
 }
 
-func (c *handler) getSpotifyToken() (string, error) {
+func (vp *videoPlaylist) getSpotifyToken() (string, error) {
 	var err error
 
 	log.Print("fetching spotify token")
@@ -264,7 +342,7 @@ func (c *handler) getSpotifyToken() (string, error) {
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
 	enc := base64.StdEncoding.EncodeToString(
-		[]byte(fmt.Sprintf("%s:%s", c.spotifyClientID, c.spotifyClientSecret)),
+		[]byte(fmt.Sprintf("%s:%s", vp.cfg.spotifyClientID, vp.cfg.spotifyClientSecret)),
 	)
 
 	req.Header.Add("Authorization", fmt.Sprintf("Basic %s", enc))
@@ -297,32 +375,41 @@ func (c *handler) getSpotifyToken() (string, error) {
 
 	// Record the time that we successfully got this token.
 	now := time.Now()
-	c.tokenSetAt = &now
+	vp.tokenSetAt = &now
 
 	return tokenRes.Token, nil
 }
 
-func (c *handler) getPlaylistSongs(
+func (vp *videoPlaylist) getPlaylistSongs(
 	plURI string,
 	offset int,
 ) ([]string, bool, error) {
 	var err error
 
-	log.Printf("requesting songs by playlist uri %s page %d, page size %d", plURI, offset/limit+1, limit)
+	if err = vp.setPlaylistReqAuth(); nil != err {
+		return []string{}, false, err
+	}
+
+	log.Printf("requesting songs by playlist uri %s page %d, page size %d", plURI, offset/pageSize+1, pageSize)
 
 	v := url.Values{}
 	v.Add("fields", "items.track(name, artists),error,next,previous")
 	v.Add("offset", fmt.Sprintf("%d", offset))
-	v.Add("limit", fmt.Sprintf("%d", limit))
+	v.Add("limit", fmt.Sprintf("%d", pageSize))
 	encoded := v.Encode()
 
 	client := http.Client{}
-	req, err := http.NewRequest(
+
+	var req *http.Request
+	if req, err = http.NewRequest(
 		"GET",
 		fmt.Sprintf("%s/playlists/%s/tracks?%s", spotifyAPIBase, plURI, encoded),
 		nil,
-	)
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", c.token))
+	); nil != err {
+		return []string{}, false, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", vp.token))
 
 	var res *http.Response
 	if res, err = client.Do(req); nil != err {
@@ -366,29 +453,53 @@ func (c *handler) getPlaylistSongs(
 	return songs, hasNextPage, nil
 }
 
-func (c *handler) buildVideos(songs []string) []video {
+func (vp *videoPlaylist) setPlaylistReqAuth() error {
+	var err error
+	log.Println("checking on get playlist auth token")
+
+	// Request an oauth token if necessary.
+	//
+	// At the time of writing, these tokens last 60min.
+	// However, should check to see if there's a way to get
+	// an exact expiration from the API.
+	now := time.Now()
+
+	if nil != vp.tokenSetAt && now.Sub(*vp.tokenSetAt) < 60*time.Minute {
+		tokenAge := now.Sub(*vp.tokenSetAt).Minutes()
+		log.Printf("reusing %.0fmin old token, will expire in %.0fmin", tokenAge, 60-tokenAge)
+		return nil
+	}
+
+	if vp.token, err = vp.getSpotifyToken(); nil != err {
+		return err
+	}
+
+	return nil
+}
+
+func (vp *videoPlaylist) buildSearchURLs(songs []string) []video {
 	var videos []video
 	for _, song := range songs {
 		videos = append(videos, video{
 			Name:      song,
-			SearchURL: buildVideoURL(song, c.mode),
+			SearchURL: buildVideoURL(song, vp.cfg.searchMode),
 		})
 	}
 
 	return videos
 }
 
-func buildVideoURL(songName string, mode string) string {
+func buildVideoURL(songName string, searchMode string) string {
 	v := url.Values{}
 
-	switch mode {
-	case modeMusicVideo:
+	switch searchMode {
+	case searchModeMusicVideo:
 		v.Add("search_query", fmt.Sprintf("%s music video", songName))
-	case modeLive:
+	case searchModeLive:
 		v.Add("search_query", fmt.Sprintf("%s live", songName))
-	case modeCover:
+	case searchModeCover:
 		v.Add("search_query", fmt.Sprintf("%s cover", songName))
-	case modeShit:
+	case searchModeShit:
 		v.Add("search_query", fmt.Sprintf("%s harry potter amv", songName))
 	}
 
